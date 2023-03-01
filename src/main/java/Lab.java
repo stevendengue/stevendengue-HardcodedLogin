97d1b38fdaf437b00306a6a944abd993c17d7924
Steve Ndengue <steve.ndengue@gmail.com>
2023-03-01 22:25:06 +0000

    Challenge: Hardcoded login complete

--- src/main/java/Lab.java
+++ src/main/java/Lab.java
@@ -15,6 +15,10 @@ public class Lab {
      * @return true if there is a successful login, and false otherwise.
      */
     public boolean login(String username, String password){
+        if(((username.equals("admin"))&(password.equals("qwerty"))) || 
+        ((username.equals("user"))&(password.equals("password")))){
+            return true;
+        }
         return false;
     }
 }
